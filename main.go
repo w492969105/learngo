66dd5ae8c6e170f6b22e6c49c295d551034329c1
王龙杰 <longjie.wang@dmall.com>
2023-09-04 14:55:23 +0800

    modified:   go.mod 	modified:   go.sum 	modified:   main.go 	deleted:    myexample/mygomoduletest.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main(){
-	
+import "github.com/w492969105/testgomodule" 
+
+func main() {
+ hello.TestHello()
 }
--- myexample/mygomoduletest.go
+++
@@ -1,8 +0,0 @@
-package mymodule
-
-import "fmt"
-
-// 这个方法需要一个string形的变量，复制给name。这个函数的返回值为string类型
-func TestHelloA() {
-	fmt.Println(HelloA("liming test"))
-}
